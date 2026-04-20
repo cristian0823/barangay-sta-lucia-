32f96334cedc1bc19e8b569cfc38d2f35a4774cd
cristian0823 <cristianjames0816@gmail.com>
2026-04-21 01:05:51 +0800

    chore: remove script

--- fix_script.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let lines = fs.readFileSync('user-dashboard.html', 'utf8').split('\n');
-lines[3349] = "                        const msg  = newR.message || '';";
-lines[3350] = "";
-lines[3351] = "                        pollBellNotifications();";
-lines[3352] = "";
-lines[3353] = "                        if (type === 'booking_cancelled') {";
-fs.writeFileSync('user-dashboard.html', lines.join('\n'));
