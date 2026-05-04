277a751edccd438bcbb60cb4f1474e48666aad70
cristian0823 <cristianjames0816@gmail.com>
2026-05-04 21:15:10 +0800

    fix: revert logo filename to barangay.jpg to fix broken links

--- patch_user_portal_image.js
+++ patch_user_portal_image.js
@@ -3,8 +3,8 @@ let html = fs.readFileSync('user-portal/user-dashboard.html', 'utf8');
 
 // Fix 1: Equipment grid - replace local imageName with image_url
 html = html.replace(
-    "const imageName = item.name ? '../' + item.name.toLowerCase().replace(/s$/, '') + '.jpg' : '../BARANGAY LOGO.jpg';",
-    "const imageSrc = item.image_url ? item.image_url : '../BARANGAY LOGO.jpg';"
+    "const imageName = item.name ? '../' + item.name.toLowerCase().replace(/s$/, '') + '.jpg' : '../barangay.jpg';",
+    "const imageSrc = item.image_url ? item.image_url : '../barangay.jpg';"
 );
 
 // Fix 2: Update the img src tag to use imageSrc and object-cover
@@ -14,8 +14,8 @@ html = html.replace(
 );
 
 // Fix 3: Borrow modal - also use image_url
-const oldBorrow = "const imageName = item.name ? '../' + item.name.toLowerCase().replace(/s$/, '') + '.jpg' : '../BARANGAY LOGO.jpg';\n            document.getElementById('borrowModalImage').src = imageName;";
-const newBorrow = "const imageSrc = item.image_url ? item.image_url : '../BARANGAY LOGO.jpg';\n            document.getElementById('borrowModalImage').src = imageSrc;";
+const oldBorrow = "const imageName = item.name ? '../' + item.name.toLowerCase().replace(/s$/, '') + '.jpg' : '../barangay.jpg';\n            document.getElementById('borrowModalImage').src = imageName;";
+const newBorrow = "const imageSrc = item.image_url ? item.image_url : '../barangay.jpg';\n            document.getElementById('borrowModalImage').src = imageSrc;";
 html = html.replace(oldBorrow, newBorrow);
 
 fs.writeFileSync('user-portal/user-dashboard.html', html);
